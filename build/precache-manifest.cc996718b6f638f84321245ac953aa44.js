fddcc6b848fa7bb25e0be7594f429ac4b5aa748e
ChrisMcMahon123 <mcmah113@uwindsor.ca>
2018-11-24 03:28:15 -0500

    Initial Commit

---
+++ build/precache-manifest.cc996718b6f638f84321245ac953aa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a26506d508eb75d031a8",
+    "url": "/static/js/main.a26506d5.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "a26506d508eb75d031a8",
+    "url": "/static/css/main.f04cc5dd.chunk.css"
+  },
+  {
+    "revision": "a6368eaf1647e2f16e93088d873a353c",
+    "url": "/index.html"
+  }
+];
